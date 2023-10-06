82f931f191c7d060ece1fcf254111685c3ba7464
wilmer-ruiz21 <wilmer_gomez23211@elpoli.edu.co>
2023-10-05 21:23:17 -0500

    crear el repository y las clases de servidores y el metodo show de ProductoController

---
+++ src/main/java/com/ppi/fagloz/controller/ProductoController.java
@@ -0,0 +1,18 @@
+package com.ppi.fagloz.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/productos")
+public class ProductoController {
+
+	@GetMapping("")
+	public String show() {
+		
+		return "productos/show";
+	}
+	
+	
+}
---
+++ src/main/java/com/ppi/fagloz/service/ProductoService.java
@@ -0,0 +1,14 @@
+package com.ppi.fagloz.service;
+
+import java.util.Optional;
+
+import com.ppi.fagloz.model.Producto;
+
+public interface ProductoService {
+	
+	public Producto save( Producto producto);
+	public Optional<Producto> get(Integer id);
+	public void update(Producto producto);
+	public void delete(Integer id);
+
+}
