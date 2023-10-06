b14d4f16b18a451413bdc7cd176571a453a2dd4c
wilmer-ruiz21 <wilmer_gomez23211@elpoli.edu.co>
2023-10-05 21:59:10 -0500

    Funcionalidad guardar producto, vista create y logger para la clase ProductoController

--- src/main/java/com/ppi/fagloz/controller/ProductoController.java
+++ src/main/java/com/ppi/fagloz/controller/ProductoController.java
@@ -1,12 +1,25 @@
 package com.ppi.fagloz.controller;
 
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ppi.fagloz.model.Producto;
+import com.ppi.fagloz.model.Usuario;
+import com.ppi.fagloz.service.ProductoService;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
+	
+	private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
+	
+	
+	@Autowired
+	private ProductoService productoService;
 
 	@GetMapping("")
 	public String show() {
@@ -14,5 +27,20 @@ public class ProductoController {
 		return "productos/show";
 	}
 	
+	@GetMapping("/create")
+	public String create(){
+		
+		return "productos/create";
+	}
+	
+	
+	@PostMapping("/save")
+	public String save(Producto producto) {
+		LOGGER.info("Este es el objeto producto {}", producto);
+		Usuario u= new Usuario(1, "", "", "", "", "", "", "");
+		producto.setUsuario(u);
+		productoService.save(producto);
+		return "redirect:/productos";
+	}
 	
 }
