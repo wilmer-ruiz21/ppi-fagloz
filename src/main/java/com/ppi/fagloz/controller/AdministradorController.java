72ec1936bc4ecdc174d38843e90f1794dac1e8a5
wilmer-ruiz21 <wilmer_gomez23211@elpoli.edu.co>
2023-10-06 21:27:23 -0500

    implementar las funcionalidades de cargar imagen y mostrar imagen para el admin

--- src/main/java/com/ppi/fagloz/controller/AdministradorController.java
+++ src/main/java/com/ppi/fagloz/controller/AdministradorController.java
@@ -1,15 +1,28 @@
 package com.ppi.fagloz.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ppi.fagloz.model.Producto;
+import com.ppi.fagloz.service.ProductoService;
 
 @Controller
 @RequestMapping("/administrador")
 public class AdministradorController {
 	
+	@Autowired
+	private ProductoService producService;
+	
 	@GetMapping("")
-	public String home() {
+	public String home(Model model) {
+		
+		List<Producto> productos = producService.findAll();
+		model.addAttribute("productos", productos);
 		
 		return "administrador/home";
 	}
--- src/main/java/com/ppi/fagloz/controller/ProductoController.java
+++ src/main/java/com/ppi/fagloz/controller/ProductoController.java
@@ -58,15 +58,7 @@ public class ProductoController {
 			producto.setImagen(nombreImagen);
 			
 		}else {
-			if (file.isEmpty()) { //editando el producto pero no cambiamos la imagen
-				Producto p = new Producto();
-				p = productoService.get(producto.getId()).get();
-				producto.setImagen(p.getImagen());
-				
-			} else {
-				String nombreImagen = upload.saveImage(file);
-				producto.setImagen(nombreImagen);
-			}
+		
 		}
 		
 		
@@ -87,7 +79,27 @@ public class ProductoController {
 	}
 	
 	@PostMapping("/update")
-	public String update(Producto producto) {
+	public String update(Producto producto,  @RequestParam("img") MultipartFile file) throws IOException {
+		Producto p = new Producto();
+		p = productoService.get(producto.getId()).get();
+		
+		
+		
+		if (file.isEmpty()) { //editando el producto pero no cambiamos la imagen
+			
+			producto.setImagen(p.getImagen());
+			
+		} else { //Cuando se edita la imagen
+			
+			//Eliminar cuando no sea la imgen por defecto
+			if (!p.getImagen().equals("default.jpg")) {
+				upload.deleteImage(p.getImagen());
+			}
+			
+			String nombreImagen = upload.saveImage(file);
+			producto.setImagen(nombreImagen);
+		}
+		producto.setUsuario(p.getUsuario());
 		productoService.update(producto);
 		return "redirect:/productos";
 	}
@@ -95,6 +107,15 @@ public class ProductoController {
 	
 	@GetMapping("/delete/{id}")
 	public String delete(@PathVariable Integer id) {
+		
+		Producto p = new Producto();
+		p = productoService.get(id).get();
+		
+		//Eliminar cuando no sea la imgen por defecto
+		if (!p.getImagen().equals("default.jpg")) {
+			upload.deleteImage(p.getImagen());
+		}
+		
 		
 		productoService.delete(id);
 		
