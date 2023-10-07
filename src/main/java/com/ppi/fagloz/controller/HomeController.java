5983d5aecd63ee8581f17320924433c7003b914b
wilmer-ruiz21 <wilmer_gomez23211@elpoli.edu.co>
2023-10-06 23:06:50 -0500

    obtener la cantidad y buscar el producto en el homecontroller para añadir al carrito

--- src/main/java/com/ppi/fagloz/controller/HomeController.java
+++ src/main/java/com/ppi/fagloz/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.ppi.fagloz.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -11,7 +13,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.ppi.fagloz.model.DetalleOrden;
+import com.ppi.fagloz.model.Orden;
 import com.ppi.fagloz.model.Producto;
 import com.ppi.fagloz.service.ProductoService;
 
@@ -23,6 +28,12 @@ public class HomeController {
 	
 	@Autowired
 	private ProductoService productoService;
+	
+	//Almacenar los detalles de la orden
+	List<DetalleOrden> detalles = new ArrayList<DetalleOrden>();
+	
+	//Datos de la orden
+	Orden orden = new Orden();
 	
 	@GetMapping("")
 	public String home(Model model) {
@@ -43,7 +54,15 @@ public class HomeController {
 	}
 	
 	@PostMapping("/cart")
-	public String addCart() {
+	public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad) {
+		DetalleOrden detalleOrden = new DetalleOrden();
+		Producto producto = new Producto();
+		double sumaTotal = 0;
+		
+		Optional<Producto> optionalProducto = productoService.get(id);
+		log.info("Producto añadido: {}", optionalProducto.get());
+		log.info("Cantidad: {}", cantidad);
+		
 		return "usuario/carrito";
 	}
 
