254a6cc95c988670d8556ba255e064137c44e6b2
wilmer-ruiz21 <wilmer_gomez23211@elpoli.edu.co>
2023-10-06 22:46:18 -0500

    mostrar información del producto en la página productohome

--- src/main/java/com/ppi/fagloz/controller/HomeController.java
+++ src/main/java/com/ppi/fagloz/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.ppi.fagloz.controller;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ppi.fagloz.model.Producto;
 import com.ppi.fagloz.service.ProductoService;
 
 @Controller
@@ -28,8 +31,13 @@ public class HomeController {
 	}
 	
 	@GetMapping("productohome/{id}")
-	public String productoHome( @PathVariable Integer id) {
+	public String productoHome( @PathVariable Integer id, Model model) {
 		log.info("Id producto enviado como parámetro {}",id);
+		Producto producto = new Producto();
+		Optional<Producto> productoOptional = productoService.get(id);
+		producto = productoOptional.get();
+		
+		model.addAttribute("producto", producto);
 		return "usuario/productohome";
 	}
 
