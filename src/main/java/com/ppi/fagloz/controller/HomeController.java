329360b9704ce152291647fd3c4bfefee9aa934a
wilmer-ruiz21 <wilmer_gomez23211@elpoli.edu.co>
2023-11-29 19:27:46 -0500

    funcionalidad para buscar un producto

--- src/main/java/com/ppi/fagloz/controller/HomeController.java
+++ src/main/java/com/ppi/fagloz/controller/HomeController.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -178,6 +180,17 @@ public class HomeController {
 		orden = new Orden();
 		detalles.clear();		
 		return "redirect:/";
+	}
+	
+	@PostMapping("/search")
+	
+	public String searchProduct(@RequestParam String nombre, Model model) {
+		log.info("Nombre del producto: {}", nombre);
+		List<Producto> productos = productoService.findAll().stream().filter( p -> p.getNombre().contains(nombre)).collect(Collectors.toList());
+		
+		model.addAttribute("productos", productos);
+		
+		return "usuario/home";
 	}
 	
 }
