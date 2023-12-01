8a57e300896482e2842544f3b8cead0be0ca088e
wilmer-ruiz21 <wilmer_gomez23211@elpoli.edu.co>
2023-12-01 13:30:05 -0500

    funcionalidad ver detalle de compra al user

--- src/main/java/com/ppi/fagloz/service/IOrdenService.java
+++ src/main/java/com/ppi/fagloz/service/IOrdenService.java
@@ -1,6 +1,7 @@
 package com.ppi.fagloz.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.ppi.fagloz.model.Orden;
 import com.ppi.fagloz.model.Usuario;
@@ -8,6 +9,8 @@ import com.ppi.fagloz.model.Usuario;
 public interface IOrdenService {
 	
 	List<Orden> findAll();
+	
+	Optional<Orden> findById(Integer id);
 	
 	Orden save (Orden orden);
 	String generarNumeroOrden();
--- src/main/java/com/ppi/fagloz/service/OrdenServiceImpl.java
+++ src/main/java/com/ppi/fagloz/service/OrdenServiceImpl.java
@@ -2,6 +2,7 @@ package com.ppi.fagloz.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -63,5 +64,10 @@ public class OrdenServiceImpl implements IOrdenService{
 	@Override
 	public List<Orden> findByUsuario(Usuario usuario) {
 		return ordenRepository.findByUsuario(usuario);
+	}
+
+	@Override
+	public Optional<Orden> findById(Integer id) {
+		return ordenRepository.findById(id);
 	}
 }
